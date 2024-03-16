bfe895cb246c1d43d21a89ef20180d722b56fef6
Zakarya <zakarya.elkhattabi@gmail.com>
2024-03-16 12:44:01 +0100

    Fix error of the unique Key in page Housing && redirect protocol in case of bad id in URL

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -2,43 +2,47 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import HousingInfo from "../../components/HousingInfo";
 import "./index.scss";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import housings from "../../data/logements.json";
 import Slideshow from "../../components/Slideshow";
 
 const Housing = () => {
+  // Id of the housing passed in the url
   let { id } = useParams();
 
-  return (
-    <>
-      <Header />
-      <main className="wrapper">
-        {housings.map((housing, index) =>
-          housing.id !== id ? (
-            ""
-          ) : (
-            <>
-              {/* Carousel */}
-              <Slideshow pictures={housing.pictures} />
+  // Redirection
+  const navigate = useNavigate();
 
-              {/* Housing info */}
-              <HousingInfo
-                key={index}
-                title={housing.title}
-                city={housing.location}
-                tags={housing.tags}
-                host={housing.host}
-                loc_rating={housing.rating}
-                description={housing.description}
-                equipments={housing.equipments}
-              />
-            </>
-          )
-        )}
-      </main>
-      <Footer />
-    </>
-  );
+  let housing_match = housings.filter((housing) => housing.id === id);
+
+  if (housing_match.length === 1 && housing_match) {
+    return (
+      <>
+        <Header />
+        <main className="wrapper">
+
+          {/* Carousel */}
+          <Slideshow pictures={housing_match[0].pictures} />
+
+          {/* Housing info */}
+          <HousingInfo
+            title={housing_match[0].title}
+            city={housing_match[0].location}
+            tags={housing_match[0].tags}
+            host={housing_match[0].host}
+            loc_rating={housing_match[0].rating}
+            description={housing_match[0].description}
+            equipments={housing_match[0].equipments}
+          />
+
+        </main>
+        <Footer />
+      </>
+    );
+  } else {
+    alert("Id inexistant");
+    navigate("/*");
+  }
 };
 
 export default Housing;
